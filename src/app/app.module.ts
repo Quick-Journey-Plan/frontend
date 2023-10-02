a5d1f930eaa39e11886bc0986b30f7b4ce8e1a52
Jeyhun Rahimli <rahimlijeyhun77@gmail.com>
2023-10-02 22:58:33 +0200

    add: dashboard layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DefaultComponent } from './shared/layout/default/default.component';
+import { DashboardComponent } from './shared/layout/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DefaultComponent
+    DefaultComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
