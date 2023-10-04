3b38a79c6a6767ac63c24296882386973998ba58
Jeyhun Rahimli <rahimlijeyhun77@gmail.com>
2023-10-04 22:58:07 +0200

    add: fontawesome free icons

--- src/app/shared/components/info/info.component.ts
+++ src/app/shared/components/info/info.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-info',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./info.component.scss']
 })
 export class InfoComponent {
-
+  public faCoffee = faCoffee;
 }
