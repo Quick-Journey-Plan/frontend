513416769272520235eb77dec6ece03f83774bf7
Jeyhun Rahimli <rahimlijeyhun77@gmail.com>
2023-10-07 20:07:14 +0200

    update: imprint page

--- src/app/pages/imprint/imprint.component.ts
+++ src/app/pages/imprint/imprint.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Content, Image } from '@app/shared/interfaces';
 
 @Component({
   selector: 'app-imprint',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./imprint.component.scss']
 })
 export class ImprintComponent {
+  @Input()
+  public content: Content = {
+    title: "Imprint", description: `
+    This website was set up by Jeyhun Rahimli for the learning project. <br /> 
+    Name: <b>Jeyhun Rahimli</b> <br />
+    Email: <a href='mailto:mail@rahimli.net'>mail@rahimli.net</a> <br />
+    Website: <a href="https://rahimli.net">rahimli.net</a>` };
 
+  @Input()
+  public image: Image = { src: 'assets/images/airplain_2.png', alt: '' };
 }
