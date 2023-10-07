796b0eac7697cc18fb85be309fe149b2b64cf3d5
Jeyhun Rahimli <rahimlijeyhun77@gmail.com>
2023-10-07 11:21:07 +0200

    add: content interface

--- src/app/shared/components/text-media/text-media.component.ts
+++ src/app/shared/components/text-media/text-media.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Content } from '@app/shared/interfaces/index';
 
 @Component({
   selector: 'app-text-media',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./text-media.component.scss']
 })
 export class TextMediaComponent {
+  @Input()
+  public content: Content = { title: '', description: '' };
 
 }
